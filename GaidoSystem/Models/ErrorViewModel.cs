97ae4798ab2e1ce2e5dc6ae0c04d68504241784f
leonidasnic <46174288+leonidasnic@users.noreply.github.com>
2019-08-06 19:45:29 -0600

    Trabajo Final de Modulo Creado

---
+++ GaidoSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GaidoSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
